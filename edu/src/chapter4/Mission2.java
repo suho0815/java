ffe3b1b2cb67221a95f7b26ec12437d98148ce21
suho0815 <50311505+suho0815@users.noreply.github.com>
2023-04-05 22:15:30 +0900

    04.05 java Mission2 수정

--- edu/src/chapter4/Mission2.java
+++ edu/src/chapter4/Mission2.java
@@ -16,26 +16,34 @@ public class Mission2 {
 		int[][] arrC = new int[3][3]; // 행렬 곱의 연산 결과 값
 		
 		
-		for(int i = 0; i < arrA[0].length; i++) {
-			int temp = 0;
-			for(int j = 0; j <arrB.length; j++) {
-				temp = temp + (arrA[i][j] * arrB[j][i]);
+		for(int i = 0; i < arrA.length; i++) {
+//			int temp = 0;
+			for(int j = 0; j < arrB[0].length; j++) { // 
 				
-//				System.out.println(temp);
-				if(j == 3) {
-					arrC[i][j] = temp;
-					System.out.println(arrC[i][j]);
-				}
+				for(int k = 0; k <arrB.length; k++) { // 행렬의 곱 계산을 위한 for문 곱셈을 4번 하고 다 더함
+//					temp += (arrA[i][k] * arrB[k][j]);
 					
+					arrC[i][j] += arrA[i][k] * arrB[k][j];
+					
+				}
+//				System.out.println(temp);
+//				if(j == 3) {
+//					arrC[i][j] = temp;
+//					System.out.println(arrC[i][j]);
+//				}
+				
 			}
 			 
 		}
 		
-//		for(int i = 0; i < arrA.length; i++) {
-//			for(int j = 0; j <arrB[0].length; j++) {
-//				System.out.println(arrC[i][j]);
-//			}
-//		}
+		for(int i = 0; i < arrC.length; i++) {
+			for(int j = 0; j < arrC[0].length; j++) {
+				System.out.print(arrC[i][j]);
+				System.out.print("\t");
+			}
+			System.out.println();
+		}
+		
 		
 	}
 
