1e1f36e57b7ae589d8c4843edcc9f371543a84cf
suho0815 <50311505+suho0815@users.noreply.github.com>
2023-04-23 12:36:12 +0900

    정처기 test

--- edu/src/baekjoon/Main.java
+++ edu/src/baekjoon/Main.java
@@ -2,13 +2,42 @@ package baekjoon;
 
 import java.util.*;
 
+class Parent{
+	int x = 100;
+	
+	Parent(){
+		this(500);
+	}
+	
+	Parent(int x){
+		this.x = x;
+	}
+	int getX() {
+		return this.x;
+	}
+}
+class Child extends Parent{
+	int x = 1000;
+	
+	Child(){
+		this(5000);
+	}
+	
+	Child(int x){
+		this.x= x;
+	}
+	
+	int getX(int n) {
+		return n;
+	}
+}
+
+
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String id = sc.nextLine();
-		
-		System.out.println(id + "??!");
+		Child obj = new Child();
+		System.out.println(obj.getX());
 		
 	}
 
